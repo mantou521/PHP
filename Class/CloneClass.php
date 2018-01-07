b836a298fda91a2f7ac7f8712f37405557d8333f
hushufeng <mantou5188@gmail.com>
2018-01-07 21:48:30 +0800

    learn class __clone

---
+++ Class/CloneClass.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hushufeng
+ * Date: 2018/1/7
+ * Time: 下午9:36
+ */
+
+namespace Classes;
+
+
+class SubObject
+{
+    static $instances = 0;
+    public $instance;
+
+    public function __construct()
+    {
+        $this->instance = ++self::$instances;
+    }
+
+    public function __clone()
+    {
+        $this->instance = ++self::$instances;
+    }
+}
+
+class MyCloneable
+{
+    public $object1;
+    public $object2;
+
+    function __clone()
+    {
+        $this->object1 = clone $this->object1;
+    }
+}
+
+$obj = new MyCloneable();
+
+$obj->object1 = new SubObject();
+$obj->object2 = new SubObject();
+
+$obj2 = clone $obj;
+
+
+print("Original Object:\n");
+print_r($obj);
+
+print("Cloned Object:\n");
+print_r($obj2);
