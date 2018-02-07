b57a1b2eef8a12e07bcbe07876d07c4b499097aa
hushufeng <shufeng.hu@seekfangle.com>
2018-02-07 09:31:12 +0800

    tmp

--- algorithm/Sort.php
+++ algorithm/Sort.php
@@ -33,12 +33,52 @@ class Sort
         $a = $b;
         $b = $c;
     }
+
+    /**
+    * 全排列
+    */
+    public function allArranged($array, $str)
+    {
+        $count = count($array);
+        if ($count == 1) {
+            echo $str . $array[0] . "\n";
+        } else {
+            for ($i=0; $i < count($array); $i++) { 
+                $tmp = $array[0];
+                $array[0] = $array[$i];
+                $array[$i] = $tmp;
+                self::allArranged(array_slice($array, 1), $str . $array[0]);
+            }
+        }
+
+    }
 }
+
+
+$Sort = new Sort();
+
+#...............交换
+echo "交换\n";
 
 $a = 1;
 $b = 2;
 
-$Sort = new Sort();
 $Sort->swap($a, $b);
 
 var_dump($a, $b);
+
+
+#............  全排列
+
+echo "\n全排列\n";
+
+$a = ['1', '2', '3', '4'];
+$Sort->allArranged($a, '');
+
+
+
+
+
+
+
+
